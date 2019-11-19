e1cc499b46bb4612c6bbf0d7ee50466fbea6beb3
Jennifer Nordwall <chikuba@users.noreply.github.com>
2019-11-19 23:04:39 +0100

    Update Ray.cpp

--- Ray.cpp
+++ Ray.cpp
@@ -3,13 +3,13 @@
 #endif
 
 Ray::Ray() {
-	this->position_v  = Vec3();
-	this->direction_v = Vec3(0.0f, 0.0f, 1.0f);
+    this->position_v  = Vec3();
+    this->direction_v = Vec3(0.0f, 0.0f, 1.0f);
 }
 
 Ray::Ray(const Vec3 &position, const Vec3 &direction) {
-	this->position_v  = position;
-	this->direction_v = direction;
+    this->position_v  = position;
+    this->direction_v = direction;
 }
 
 Ray::~Ray() {}
