6ec33be29d9959693f4d952c2ab176ebafabe39d
Jennifer Nordwall <chikuba@users.noreply.github.com>
2019-11-19 23:06:35 +0100

    Update Ray.h

--- Ray.h
+++ Ray.h
@@ -7,35 +7,35 @@
 
 class Ray {
     private:
-		Vec3 position_v, direction_v;
-	//
-	public:
-		Ray();
-		Ray(const Vec3 &position, const Vec3 &direction);
-		~Ray();
+        Vec3 position_v, direction_v;
+    //
+    public:
+        Ray();
+        Ray(const Vec3 &position, const Vec3 &direction);
+        ~Ray();
 		
-		Vec3 GetPosition() const;
-		void SetPosition(const Vec3 &new_position);
+        Vec3 GetPosition() const;
+        void SetPosition(const Vec3 &new_position);
 	
-		Vec3 GetDirection() const;
-		void SetDirection(const Vec3 &new_direction);
-	//
+        Vec3 GetDirection() const;
+        void SetDirection(const Vec3 &new_direction);
+    //
 };
 
 #endif
 
 inline Vec3 Ray::GetPosition() const {
-	return this->position_v;
+    return this->position_v;
 }
 
 inline void Ray::SetPosition(const Vec3 &new_position) {
-	this->position_v = new_position;
+    this->position_v = new_position;
 }
 
 inline Vec3 Ray::GetDirection() const {
-	return this->direction_v;
+    return this->direction_v;
 }
 
 inline void Ray::SetDirection(const Vec3 &new_direction) {
-	this->direction_v = new_direction;
+    this->direction_v = new_direction;
 }
