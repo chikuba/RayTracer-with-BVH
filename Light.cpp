bacc2d6ae94f1e58f7a1133638e7109b78f010cc
Jennifer Nordwall <chikuba@users.noreply.github.com>
2019-11-19 23:01:20 +0100

    Update Light.cpp

--- Light.cpp
+++ Light.cpp
@@ -3,13 +3,13 @@
 #endif
 
 Light::Light() {
-	this->position_v  = Vec3(1.0f, 1.0f, 1.0f);
-	this->intensity_c = ColorRGB(150.0f, 0.0f, 0.0f);
+    this->position_v  = Vec3(1.0f, 1.0f, 1.0f);
+    this->intensity_c = ColorRGB(150.0f, 0.0f, 0.0f);
 }
 
 Light::Light(const Vec3 &position, const ColorRGB &intensity) {
-	this->position_v  = position;
-	this->intensity_c = intensity;
+    this->position_v  = position;
+    this->intensity_c = intensity;
 }
 
 Light::~Light() {}
