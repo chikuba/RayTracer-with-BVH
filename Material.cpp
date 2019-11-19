a638acad7ede35d3f2b85ff3609e7d9c17f60f35
Jennifer Nordwall <chikuba@users.noreply.github.com>
2019-11-19 23:02:20 +0100

    Update Material.cpp

--- Material.cpp
+++ Material.cpp
@@ -3,17 +3,17 @@
 #endif
 
 Material::Material() {
-	this->diffuse_c    = ColorRGB(0.3f, 0.3f, 1.0f);
-	this->specular_c   = ColorRGB(1.0f, 1.0f, 1.0f);
-	this->reflection_f = 0.7f;
-	this->power_usi    = 60;
+    this->diffuse_c    = ColorRGB(0.3f, 0.3f, 1.0f);
+    this->specular_c   = ColorRGB(1.0f, 1.0f, 1.0f);
+    this->reflection_f = 0.7f;
+    this->power_usi    = 60;
 }
 
 Material::Material(const ColorRGB &diffuse, const ColorRGB &specular, const float &reflection, const unsigned short int &power) {
-	this->diffuse_c    = diffuse;
-	this->specular_c   = specular;
-	this->reflection_f = reflection;
-	this->power_usi	   = power;
+    this->diffuse_c    = diffuse;
+    this->specular_c   = specular;
+    this->reflection_f = reflection;
+    this->power_usi	   = power;
 }
 
 Material::~Material() {}
