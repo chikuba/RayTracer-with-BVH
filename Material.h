bd1f9e75907db1c27074df46476b0f765668da7b
Jennifer Nordwall <chikuba@users.noreply.github.com>
2019-11-19 23:03:18 +0100

    Update Material.h

--- Material.h
+++ Material.h
@@ -7,37 +7,37 @@
 
 class Material {
     private:
-		ColorRGB diffuse_c, specular_c;
-		float reflection_f;
-		unsigned short int power_usi;
-	//
-	public:
-		Material();
-		Material(const ColorRGB &diffuse, const ColorRGB &specular, const float &reflection, const unsigned short int &power);
-		~Material();
+        ColorRGB diffuse_c, specular_c;
+        float reflection_f;
+        unsigned short int power_usi;
+    //
+    public:
+        Material();
+        Material(const ColorRGB &diffuse, const ColorRGB &specular, const float &reflection, const unsigned short int &power);
+        ~Material();
 		
-		ColorRGB GetDiffuse() const;
-		ColorRGB GetSpecular() const;
-		float GetReflection() const;
-		unsigned short int GetPower() const;
-	//
+        ColorRGB GetDiffuse() const;
+        ColorRGB GetSpecular() const;
+        float GetReflection() const;
+        unsigned short int GetPower() const;
+    //
 };
 
 #endif
 
 
 inline ColorRGB Material::GetDiffuse() const {
-	return this->diffuse_c;
+    return this->diffuse_c;
 }
 
 inline ColorRGB Material::GetSpecular() const {
-	return this->specular_c;
+    return this->specular_c;
 }
 
 inline float Material::GetReflection() const {
-	return this->reflection_f;
+    return this->reflection_f;
 }
 
 inline unsigned short int Material::GetPower() const {
-	return this->power_usi;
+    return this->power_usi;
 }
