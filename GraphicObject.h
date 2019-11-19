31d57a710aca9f4f76866cbda08098812c4d82d4
Jennifer Nordwall <chikuba@users.noreply.github.com>
2019-11-19 23:00:55 +0100

    Update GraphicObject.h

--- GraphicObject.h
+++ GraphicObject.h
@@ -13,27 +13,27 @@
 
 class GraphicObject {
     protected:
-		Vec3 position_v, side_v;
-		Material shader_m;
-	//
+        Vec3 position_v, side_v;
+        Material shader_m;
+    //
     public:
-		GraphicObject();
-		GraphicObject(const Vec3 &position, const Vec3 &side);
-		GraphicObject(const Vec3 &position, const Vec3 &side, const Material &shader);
-		virtual ~GraphicObject();
+        GraphicObject();
+        GraphicObject(const Vec3 &position, const Vec3 &side);
+        GraphicObject(const Vec3 &position, const Vec3 &side, const Material &shader);
+        virtual ~GraphicObject();
 		
-		virtual Vec3 GetMin() = 0;
-		virtual Vec3 GetMid() = 0;
-		virtual Vec3 GetMax() = 0;
-		virtual Vec3 GetNormalAt(Vec3 &intersection_point) = 0;
+        virtual Vec3 GetMin() = 0;
+        virtual Vec3 GetMid() = 0;
+        virtual Vec3 GetMax() = 0;
+        virtual Vec3 GetNormalAt(Vec3 &intersection_point) = 0;
 		
-		Material GetMaterial() const;
+        Material GetMaterial() const;
 	
-		virtual bool GetSideOfCenter(GraphicObject *go_p) { return false; }
-		virtual bool Intersect(const Ray &view_ray, float &distance_t) { return false; }
-		virtual unsigned short int Type() const { return 0; }
-		virtual void Print() const;
-	//
+        virtual bool GetSideOfCenter(GraphicObject *go_p) { return false; }
+        virtual bool Intersect(const Ray &view_ray, float &distance_t) { return false; }
+        virtual unsigned short int Type() const { return 0; }
+        virtual void Print() const;
+    //
 };
 
 #endif
