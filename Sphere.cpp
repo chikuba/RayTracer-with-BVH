ecf72b020549b4e8b581f18152db87df2085fc2f
Jennifer Nordwall <chikuba@users.noreply.github.com>
2019-11-19 23:07:30 +0100

    Update Sphere.cpp

--- Sphere.cpp
+++ Sphere.cpp
@@ -13,33 +13,32 @@ Sphere::Sphere(const Vec3 &position, const float &radie, const Material &shader)
 Sphere::~Sphere() {}
 
 Vec3 Sphere::GetNormalAt(Vec3 &intersection_point_v) {
-	float temp_f = (intersection_point_v - this->position_v) * (intersection_point_v - this->position_v);
+    float temp_f = (intersection_point_v - this->position_v) * (intersection_point_v - this->position_v);
 	
-	if(temp_f != 0.0f) { 
-		temp_f = Math::InvSqrt(temp_f); 
-		return (intersection_point_v - this->position_v) * temp_f;
-	} else return Vec3();
+    if(temp_f != 0.0f) { 
+        temp_f = Math::InvSqrt(temp_f); 
+        return (intersection_point_v - this->position_v) * temp_f;
+    } else return Vec3();
 }
 
 // a simple ray - sphere intersection by using the spheres center and radius
 bool Sphere::Intersect(const Ray &view_ray, float &distance_f) { 
-	Vec3 distans = view_ray.GetPosition() - this->position_v; 
-	float b_f = distans *  view_ray.GetDirection();
-	float c_f = (distans * distans) - (this->side_v.x * this->side_v.x);
-	float d_f = (b_f * b_f) - c_f; 
+    Vec3 distans = view_ray.GetPosition() - this->position_v; 
+    float b_f = distans *  view_ray.GetDirection();
+    float c_f = (distans * distans) - (this->side_v.x * this->side_v.x);
+    float d_f = (b_f * b_f) - c_f; 
 	
-	if(((c_f > 0.0f) && (b_f > 0.0f)) || (d_f < 0.0f)) return false; 
+    if(((c_f > 0.0f) && (b_f > 0.0f)) || (d_f < 0.0f)) return false; 
 	
-	float t0_f = -b_f - Math::Sqrt(d_f); 
+    float t0_f = -b_f - Math::Sqrt(d_f); 
 	
-	if(t0_f < 0.0f) return false;
+    if(t0_f < 0.0f) return false;
 	
-	if(t0_f <= distance_f) {
-		distance_f = t0_f; // the distans for shading purposes
+    if(t0_f <= distance_f) {
+        distance_f = t0_f; // the distans for shading purposes		
+        return true;
 		
-		return true;
-		
-	} else return false;
+    } else return false;
 }
 
 void Sphere::Print() const {
